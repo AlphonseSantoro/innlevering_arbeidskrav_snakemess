e590dab7fdb387af3076d41772af2d48180d946e
MrMokka <sartaren@hotmail.com>
2018-09-28 13:55:39 +0200

    Created GameController File

---
+++ SnakeMess/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess {
+	class GameController {
+
+		public static readonly int windowWidth = Console.WindowWidth;
+		public static readonly int windowHeight = Console.WindowHeight;
+		public static bool gameOver = false;
+		public static bool gamePaused = false;
+
+		private Snake _snake;
+		private Food _food;
+
+		GameController() {
+
+		}
+
+
+
+
+	}
+}
