360b78ea6f9593edee8352fc4ce1cb530cbe0610
Alexander <breale17@egms.no>
2018-09-28 15:11:53 +0200

    .

---
+++ SnakeMess/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess {
+    class Food : Item {
+
+        public Food(int x, int y, Marker marker) : base(x, y, marker) { }
+    }
+
+
+}
